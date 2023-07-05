a5f7261bdf71b1d74f1b0cad55e3f6d2e22c8b6e
JekaH1 <ceyhun100203@gmail.com>
2023-07-06 02:03:40 +0400

    modals done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
+// @ts-nocheck
+
 const sliderPopUps = document.querySelectorAll('.slider-tag');
 const popUpImage = document.querySelector('.popup-image');
 const popUp = document.querySelector('.popup');
 const modalPrevBtn = document.querySelector('.modal-prev-btn');
 const modalNextBtn = document.querySelector('.modal-next-btn');
+const slider = document.querySelector('.images-slider');
 
 
-console.log(popUpImage);
 sliderPopUps.forEach(element => {
     element.addEventListener('click', function (e) {
         e.preventDefault();
+        console.log(element);
         popModal(element);
 
     })
@@ -16,9 +19,10 @@ sliderPopUps.forEach(element => {
 
 function popModal(element) {
     popUpImage.setAttribute('src', element.getAttribute('href'))
-    console.log(element);
     popUp.classList.remove('d-none');
     popUp.classList.add('active');
+    prevModal(element);
+    nextModal(element);
 }
 
 function popDownModal() {
@@ -28,5 +32,21 @@ function popDownModal() {
         }
     })
 }
+function prevModal(activeModal) {
+    modalPrevBtn.addEventListener('click', function (e) {
+        if (activeModal.previousElementSibling) {
+            popModal(activeModal.previousElementSibling);
+        }
+    })
+}
+function nextModal(activeModal) {
+    modalNextBtn.addEventListener('click', function (e) {
+        if (activeModal.nextElementSibling) {
+            popModal(activeModal.nextElementSibling);
+        }
+    })
+}
+
 popDownModal();
+
 
